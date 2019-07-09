7f97eb34ca3ce1018ab7fc715bc807da3c4486f5
Hugh Sturrock <hughsturrock@gmail.com>
2019-07-08 22:08:17 -0700

    Simpler UI

--- server.R
+++ server.R
@@ -1,41 +1,50 @@
 library(rsconnect)
 library(downloader)
 library(NCmisc)
+library(stringr)
 
 shinyServer(function(input, output) {
   
   # First remove any server or ui files that exist on server
-  file.remove("to_deploy/ui.R")
-  file.remove("to_deploy/server.R")
-  
-  # get all files in the directories, recursively
-  f <- list.files("to_deploy", include.dirs = F, full.names = T, recursive = T)
+  f <- list.files("to_deploy", include.dirs = T, full.names = T, recursive = T)
   # remove the files
   file.remove(f)
   
-
   observeEvent(input$deploy,{
     
     withProgress(message = 'Deploying your app',{
+
+    # Get repo files
+    repo <- paste0("https://raw.githubusercontent.com/disarm-platform/", input$repo, "/master/")
     
     # Download server and ui files
-    download(input$ui,
+    download(paste0(repo, "ui.R"),
              destfile = "to_deploy/ui.R")
-    download(input$server,
+    download(paste0(repo, "server.R"),
              destfile = "to_deploy/server.R")
     
-    # Define shiny app account
-    # shiny_inputs <- list(name=input$account, 
-    #  token=input$token, 
-    #  secret=input$secret)
-  
-    rsconnect::setAccountInfo(input$account,
-                              token=input$token,
-                              input$secret)
+    # Download utils.R if it exists
+    try({
+      download(paste0(repo, "utils.R"), destfile = "to_deploy/utils.R")},
+      silent = TRUE)
+    
+    # Download css file if exists
+    try({
+      download(paste0(repo, "styles.css"), destfile = "to_deploy/styles.css")},
+        silent = TRUE)
+    
+    commas <- str_locate_all(input$account, "'")[[1]][,1]
+    account <- substr(input$account, commas[1]+1, commas[2]-1)
+    token <- substr(input$account, commas[3]+1, commas[4]-1)
+    secret <- substr(input$account, commas[5]+1, commas[6]-1)
+    
+    rsconnect::setAccountInfo(account,
+                              token,
+                              secret)
     
     deployApp(appDir = "to_deploy",
               appName = input$app_name,
-              account = input$account)
+              account = account)
     
     })
     
--- ui.R
+++ ui.R
@@ -11,13 +11,10 @@ dashboardPage(
                     
                   p("This app allows you to deploy existing shiny app source code. All you need 
                   is a shinyapps account and links to the ui.R and server.R scripts"),
-                  textInput("ui", "Link to ui.R"),
-                  textInput("server", "Link to server.R"),
+                  textInput("repo", "Repository name", placeholder = "e.g. ui-village-finder"),
                   textInput("algo", "Algorithm URL", placeholder = "Not needed right now.."),
                   textInput("app_name", "App name"),
-                  textInput("account", "shinyapps account"),
-                  textInput("token", "shinyapps token"),
-                  textInput("secret", "shinyapps secret"),
+                  textInput("account", "Shinyapps Tokens", placeholder = "Hint: starts with 'rsconnect::setAccountInfo' "),
                   actionButton("deploy", "Deploy app")
                   )
                 ))

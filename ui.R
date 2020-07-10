03d88dce0d8c259450a062f54802cbc2705abaa6
Hugh Sturrock <hughsturrock@gmail.com>
2020-07-10 11:34:16 -0700

    update instructions

--- ui.R
+++ ui.R
@@ -10,7 +10,7 @@ dashboardPage(
                   box(
                     
                   p("This app allows you to deploy existing shiny app source code. All you need 
-                  is a shinyapps account and links to the ui.R and server.R scripts"),
+                  is a shinyapps account and link to the GitHub repo with the relevant ui.R and server.R scripts"),
                   textInput("repo", "Repository name", placeholder = "e.g. ui-village-finder"),
                   textInput("algo", "Algorithm URL", placeholder = "Not needed right now.."),
                   textInput("app_name", "App name"),
